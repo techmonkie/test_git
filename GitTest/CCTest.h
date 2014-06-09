cb7d9faa4b0924f8691d21ba9a503718c8b36b2d
Sean Rodgers <techmonkie@Seans-MacBook-Pro.local>
2014-06-09 15:38:44 -0400

    added a test class

---
+++ GitTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Sean Rodgers on 6/9/14.
+//  Copyright (c) 2014 TechMonkie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GitTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Sean Rodgers on 6/9/14.
+//  Copyright (c) 2014 TechMonkie. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
